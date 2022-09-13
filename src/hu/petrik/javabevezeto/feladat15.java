a6389f7082f6f21eb1a7984bbbb532974a3b6450
Bóta László Levente <bota.laszlo.levente@petrik.hu>
2022-09-13 16:20:03 +0200

    f15 tomb feltoltes

--- src/hu/petrik/javabevezeto/feladat15.java
+++ src/hu/petrik/javabevezeto/feladat15.java
@@ -3,6 +3,8 @@ package hu.petrik.javabevezeto;
 public class feladat15 {
     public static void main(String[] args) {
         double[] tomb = new double[25];
-
+        for (int i = 0; i < tomb.length; i++) {
+            tomb[i]=Math.random()*200-100; //-100 és 100 között random szám
+        }
     }
 }
