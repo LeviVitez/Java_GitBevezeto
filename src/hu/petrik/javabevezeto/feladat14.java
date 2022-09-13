52be0aca1385e7e891b1db0316397c2dae43aca7
Bóta László Levente <bota.laszlo.levente@petrik.hu>
2022-09-13 15:37:00 +0200

    f14

---
+++ src/hu/petrik/javabevezeto/feladat14.java
@@ -0,0 +1,11 @@
+package hu.petrik.javabevezeto;
+
+import java.util.Scanner;
+
+public class feladat14 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Kérek egy szöveget: ");
+        String szoveg=sc.nextLine();
+    }
+}
