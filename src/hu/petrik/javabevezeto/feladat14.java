be195297972916e4970aee123da64fae4ee5b61c
Bóta László Levente <bota.laszlo.levente@petrik.hu>
2022-09-13 16:14:37 +0200

    f14 majdnem kesz csak szar

--- src/hu/petrik/javabevezeto/feladat14.java
+++ src/hu/petrik/javabevezeto/feladat14.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 
 public class feladat14 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Kérek egy szöveget: ");
-        String szoveg=sc.nextLine();
-        String menupont="";
-        while (menupont.equals("f")){
+        String szoveg = sc.nextLine();
+        String menupont = " ";
+        while (menupont.equals("f")) {
             System.out.println("Adja meg, hogy mit szeretne csinálni a szöveggel:");
             System.out.println("\ta - Nagybetűssé alakítani");
             System.out.println("\tb - kisbetűssé alakítani");
@@ -18,7 +18,43 @@ public class feladat14 {
             System.out.println("\te - A szöveg egy részét kiiratni");
             System.out.println("\tf - kilépni");
             System.out.println("Adja meg a menüpontot: ");
-            menupont=sc.nextLine().toLowerCase();
+            menupont = sc.nextLine().toLowerCase();
+            switch (menupont) {
+                case "a":
+                    System.out.println(szoveg.toUpperCase());
+                    break;
+                case "b":
+                    System.out.println(szoveg.toLowerCase());
+                    break;
+                case "c":
+                    System.out.printf("A szöveg hossza %d karakter\n", szoveg.length());
+                    break;
+                case "d":
+                    System.out.print("Kérem adja meg a másik szöveget: ");
+                    String szoveg2 = sc.nextLine();
+                    int osszehasonlitas = szoveg.compareTo(szoveg2);
+                    if (osszehasonlitas < 0) {
+                        System.out.println("Az első szöveg van előrébb az ABC-ben");
+                    } else if (osszehasonlitas > 0) {
+                        System.out.println("A második szöveg van előrébb az ABC-ben");
+                    }
+                    break;
+                case "e":
+                    System.out.print("Kérem adja meg a kezdő karaktert sorszámát amit ki szeretne írni: ");
+                    int a = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Kérem adja meg az utlosó karakter sor számát amit ki szeretne írni");
+                    int b = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println(szoveg.substring(a - 1, b - 1));
+                    break;
+                case "f":
+                    System.out.println("Viszlát!");
+                    break;
+                default:
+                    System.out.println("Rossz lehetőséget adott meg kérem a menüpontok közül válasszon!");
+                    break;
+            }
         }
     }
 }
