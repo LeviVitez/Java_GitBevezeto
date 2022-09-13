5bf5eedbb330c06da2d082dac48b67734f6a2950
Bóta László Levente <bota.laszlo.levente@petrik.hu>
2022-09-13 15:46:11 +0200

    f14 tovabb

--- src/hu/petrik/javabevezeto/feladat14.java
+++ src/hu/petrik/javabevezeto/feladat14.java
@@ -1,5 +1,6 @@
 package hu.petrik.javabevezeto;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class feladat14 {
@@ -7,5 +8,17 @@ public class feladat14 {
         Scanner sc=new Scanner(System.in);
         System.out.print("Kérek egy szöveget: ");
         String szoveg=sc.nextLine();
+        String menupont="";
+        while (menupont.equals("f")){
+            System.out.println("Adja meg, hogy mit szeretne csinálni a szöveggel:");
+            System.out.println("\ta - Nagybetűssé alakítani");
+            System.out.println("\tb - kisbetűssé alakítani");
+            System.out.println("\tc - lekérdezni a hosszát");
+            System.out.println("\td - Összehasonlítani egy másik szöveggel");
+            System.out.println("\te - A szöveg egy részét kiiratni");
+            System.out.println("\tf - kilépni");
+            System.out.println("Adja meg a menüpontot: ");
+            menupont=sc.nextLine().toLowerCase();
+        }
     }
 }
